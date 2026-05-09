eab0fcaf1144ff7ef460a98d4de195bf25bd8a3b
Toto Sugito <toto_sugito_plg@yahoo.co.id>
2026-05-09 16:44:40 +0700

    fix minor bugs

--- src/components/app/sidebar/AppSidebar.tsx
+++ src/components/app/sidebar/AppSidebar.tsx
@@ -23,7 +23,7 @@ function AppSidebar({ navItems, ...props }: { navItems: SidebarData }) {
       {...props}
     >
       <SidebarHeader className="bg-sidebar border-b border-sidebar-border/30 py-2">
-        <AppLogo disableColapsed={true} />
+        <AppLogo disableCollapsed={true} />
       </SidebarHeader>
 
       <SidebarContent className={cn(
