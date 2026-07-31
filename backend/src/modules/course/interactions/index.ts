c8ad52da0abed93770db0ea1f28d7032a67120b1
totosugito <toto_sugito_plg@yahoo.co.id>
2026-07-31 15:41:22 +0700

    update courses list for public

--- backend/src/modules/course/interactions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./interactions.schema.ts";
-export { rateCourseService } from "./services/user/rating.service.ts";
-export { toggleBookmarkService } from "./services/user/bookmark.service.ts";
-export { toggleLikeService } from "./services/user/like.service.ts";
-export { getFavoritesService } from "./services/user/favorites.service.ts";
